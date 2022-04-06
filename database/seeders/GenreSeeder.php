627ec410345d35ee32127bb7c6a362e2c5616141
joegh <joe.gharbi@gmail.com>
2022-04-06 02:19:44 +0200

    Faker and seeder for genres table.

--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -2,19 +2,21 @@
 
 namespace Database\Seeders;
 
+use App\Models\Genre;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
-class DatabaseSeeder extends Seeder
+class GenreSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(BookSeeder::class);
-        $this->call(GenreSeeder::class);
+        DB::table('genres')->truncate();
+        Genre::factory(3)->create();
     }
 }
